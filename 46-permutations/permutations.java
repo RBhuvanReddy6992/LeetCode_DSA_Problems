4a5340490ce46f7274ad9fa368c4a4232f1bf6d4
Ranabothu Bhuvan Reddy <bhuvanreddy6994@gmail.com>
2026-04-28 12:25:43 +0530

    Time: 1 ms (85.86%) | Memory: 45.6 MB (47.29%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+   List<List<Integer>> result= new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> current= new ArrayList<>();
+       
+        return helper(nums,current);
+    }
+
+    public List<List<Integer>> helper(int [] nums,List<Integer> current){
+        if(current.size() == nums.length){
+            result.add(new ArrayList<>(current));
+            return result;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(current.contains(nums[i])){
+                continue;
+            }
+            current.add(nums[i]);
+            helper(nums,current);
+            current.remove(current.size()-1);
+        }
+        return result;
+    }
+}
